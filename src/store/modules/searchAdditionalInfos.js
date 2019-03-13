7371d37147663191f5bd91d236c7180a5ba4976c
Alexis Leclerc <alexis@alxs.fr>
2019-03-13 21:03:00 +0100

    [RNCS] search RNCS info when association

--- src/store/modules/searchAdditionalInfos.js
+++ src/store/modules/searchAdditionalInfos.js
@@ -27,12 +27,14 @@ const getters = {
 
 const actions = {
   // When we have an RNA ID, check if there is a Siret. If yes, look up Sirene. If no, look up the ID in Sirene.
-  fromRNARequestOtherAPIs(dispatch, id) {
+  async fromRNARequestOtherAPIs(dispatch, id) {
     if (store.getters.siretFromRNA) {
-      store.dispatch('executeSearchBySiret', { siret: store.getters.siretFromRNA, api: 'SIRENE' })
+      await store.dispatch('executeSearchBySiret', { siret: store.getters.siretFromRNA, api: 'SIRENE' })
     } else {
-      store.dispatch('executeSearchByIdAssociation', { id: id, api: 'SIRENE' })
+      await store.dispatch('executeSearchByIdAssociation', { id: id, api: 'SIRENE' })
     }
+
+    store.dispatch('searchAdditionalInfoSirene', 'RNCS')
   },
   // When we have Siret, check if there is an RNA ID. If yes, look up RNA. If no, look up Siret in RNA.
   // Also get additional RNCS and RNM Info if from SIRENE

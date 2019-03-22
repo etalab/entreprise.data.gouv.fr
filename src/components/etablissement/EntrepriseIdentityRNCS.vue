bcca7a025ea6255c523c553eb600ebc217aacf4c
Alexis Leclerc <alexis@alxs.fr>
2019-03-22 14:51:12 +0200

    [RNCS] remove sirene dependencies

--- src/components/etablissement/EntrepriseIdentityRNCS.vue
+++ src/components/etablissement/EntrepriseIdentityRNCS.vue
@@ -1,21 +1,17 @@
 <template>
   <section class="section">
     <div class="container">
-      <server-error v-if=isError />
-      <not-found v-else-if=isNotFound />
-      <div v-else>
-        <div class="notification error" v-if=isRNCSError>Erreur du service RNCS : {{ RNCSError }}</div>
-        <entreprise-identity-header :searchId=searchId />
-        <blocks-skeleton v-if=RNCSLoading />
-        <etablissement-rncs v-else-if=haveRNCSInfo />
-        <div v-if=haveRNCSInfo class="company__extra">
-          <div class="notification grey">
-            <div>Ces informations sont issues du RNCS mis à jour le {{ RNCSUpdate }}.</div>
-            <a class="button-outline secondary" target="_blank" v-bind:href="dataRequestURL" title="Accéder aux données brutes de cette entreprise">
-              <img class="icon" src="@/assets/img/json.svg" alt="" />
-              Accéder aux données JSON
-            </a>
-          </div>
+      <div class="notification error" v-if=isRNCSError>Erreur du service RNCS : {{ RNCSError }}</div>
+      <entreprise-identity-header :searchId=searchId />
+      <blocks-skeleton v-if=RNCSLoading />
+      <etablissement-rncs v-else-if=haveRNCSInfo />
+      <div v-if=haveRNCSInfo class="company__extra">
+        <div class="notification grey">
+          <div>Ces informations sont issues du RNCS mis à jour le {{ RNCSUpdate }}.</div>
+          <a class="button-outline secondary" target="_blank" v-bind:href="dataRequestURL" title="Accéder aux données brutes de cette entreprise">
+            <img class="icon" src="@/assets/img/json.svg" alt="" />
+            Accéder aux données JSON
+          </a>
         </div>
       </div>
     </div>
@@ -33,11 +29,6 @@ import BlocksSkeleton from '@/components/etablissement/skeletons/BlocksSkeleton'
 
 export default {
   name: 'EntrepriseIdentityRNCS',
-  metaInfo () {
-    return {
-      title: this.titleEtablissement()
-    }
-  },
   components: {
     'Loader': Loader,
     'ServerError': ServerError,
@@ -50,32 +41,17 @@ export default {
     searchId () {
       return this.$route.params.searchId
     },
-    isNotFound () {
-      return this.$store.getters.mainAPISNotFound
-    },
-    isError () {
-      return this.$store.getters.mainAPISError
-    },
-    haveSireneInfo () {
-      return this.$store.getters.sireneAvailable
-    },
     haveRNCSInfo () {
       return this.$store.getters.additionalAPIAvailable('RNCS')
     },
-    resultSirene () {
-      if (this.haveSireneInfo) {
-        return this.$store.getters.singlePageEtablissementSirene
-      }
-      return null
-    },
     dataRequestURL () {
-      if (this.resultSirene) {
-        return `${process.env.BASE_ADDRESS_RNCS}${this.resultSirene.siren}`
+      if (this.haveRNCSInfo) {
+        return `${process.env.BASE_ADDRESS_RNCS}${this.$store.getters.RNCSData.siren}`
       }
       return null
     },
     RNCSUpdate () {
-      if (this.$store.getters.RNCSData) {
+      if (this.haveRNCSInfo) {
         return Filters.filters.frenchDateFormat(this.$store.getters.RNCSData.db_current_date)
       }
       return null
@@ -88,17 +64,6 @@ export default {
     },
     RNCSError () {
       return this.$store.getters.RNCSError
-    }
-  },
-  methods: {
-    titleEtablissement () {
-      if (this.haveSireneInfo) {
-        return Filters.filters.removeExtraChars(this.$store.getters.singlePageEtablissementSirene.nom_raison_sociale)
-      } else if (this.haveRNAInfo) {
-        return this.$store.getters.singlePageEtablissementRNA.titre
-      } else {
-        return 'Etablissement'
-      }
     }
   },
   beforeCreate () {

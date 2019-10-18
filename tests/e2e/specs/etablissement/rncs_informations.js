0c10b4300a2f21121c176fd8b0f7ab9615f3ef2e
Samuelfaure <samuel.faure.dev@gmail.com>
2019-10-18 11:51:30 +0200

    fix end2end tests

--- tests/e2e/specs/etablissement/rncs_informations.js
+++ tests/e2e/specs/etablissement/rncs_informations.js
@@ -10,7 +10,7 @@ const companyNameElement =
   "#app > section > div > div:nth-child(1) > div.company > div.company-container > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div.company__item-value";
 
 const presidentNameElement =
-  "#app > section > div > div:nth-child(1) > div.company > div.company__panel.panel > div > div:nth-child(19) > div > div:nth-child(2) > div.company__item-value";
+  "#app > section > div > div:nth-child(1) > div.company > div.company__panel.panel > div > div:nth-child(9) > div > div:nth-child(2) > div.company__item-value";
 
 const observationDateElement =
   "#app > section > div > div:nth-child(1) > div.company > div:nth-child(3) > div:nth-child(3) > div.company__item.company__comment-date > div";

75c30184deb80293655ac09bb765e7450a3f154c
Alexis Leclerc <alexis@alxs.fr>
2020-02-27 15:07:34 +0200

    Add INPI links to RNCS web/pdf

--- src/components/etablissement/EtablissementSirene.vue
+++ src/components/etablissement/EtablissementSirene.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="company">
+    <etablissement-links />
+
+    <h3>Informations de la base SIRENE de l'INSEE</h3>
     <div class="company-container">
       <etablissement-sirene-contact />
       <etablissement-sirene-info />
@@ -8,12 +11,14 @@
 </template>
 
 <script>
+import EtablissementLinks from "@/components/etablissement/etablissementSirene/EtablissementLinks";
 import EtablissementSireneContact from "@/components/etablissement/etablissementSirene/EtablissementSireneContact";
 import EtablissementSireneInfo from "@/components/etablissement/etablissementSirene/EtablissementSireneInfo";
 
 export default {
   name: "EtablissementSirene",
   components: {
+    EtablissementLinks: EtablissementLinks,
     EtablissementSireneContact: EtablissementSireneContact,
     EtablissementSireneInfo: EtablissementSireneInfo
   }
@@ -21,6 +26,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h3 {
+  margin-bottom: 0;
+}
 .company {
   margin-top: 2em;
 }
